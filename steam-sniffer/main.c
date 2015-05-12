515860d58f6ff851639e92dd5160180058256794
Vasil <ludesign.bg@gmail.com>
2015-05-12 23:13:36 +0300

    initial commit of sniffer client for sniffing steam dota 2 network traffic

---
+++ steam-sniffer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  steam-sniffer
+//
+//  Created by Vasil Stoychev on 5/12/15.
+//  Copyright (c) 2015 Vasil Stoychev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	printf("steam-sniffer for dota 2!\n");
+	
+	return 0;
+}
